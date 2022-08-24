5b338679d5e1faccc48097d1539b5d04526899e2
Leonardo <leonardobeletatto9@gmail.com>
2022-08-23 22:52:28 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	contaDoLeozao := contaCorrente{
+		"Leonardo",
+		155,
+		123456,
+		125.5,
+	}
+
+	contaDoAbel := contaCorrente{
+		"Abel",
+		200,
+		202021,
+		156548.7,
+	}
+
+	fmt.Println(contaDoLeozao,"\n", contaDoAbel)
+}
+
+type contaCorrente struct{
+	titular string
+	numeroAgencia int
+	numeroConta int
+	saldo float32
+}
